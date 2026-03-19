f59fab809f5ff742a6780ccfb6335be4fe7c6a2f
doophster <carrocasl@vcu.edu>
2026-03-18 20:13:53 -0400

    ahahahahaha

--- jquery-practice/main.js
+++ jquery-practice/main.js
@@ -1,3 +1,21 @@
-$("button").click(function(){
-    $("p").toggleClass("main");
+$(".dark-btn").click(
+    function(){
+        $(".box").toggleClass("dark");
+        $(".container").toggleClass("dark");
 });
+
+$(".spin-btn").click(
+    function(){
+        $(".box").toggleClass("spin");
+    }
+);
+
+$(".reveal-btn").click(
+    function(){
+        $(".chair").addClass("show");
+       // $(".reveal-btn").css("display", "none");
+       $(".reveal-btn").hide();
+    }
+);
+
+$(".draggable").draggable();
